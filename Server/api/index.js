5aeeda265bda9e0919a04143badbcc6222cd2257
tds22 <tds22@njit.edu>
2024-04-30 16:23:15 -0400

    message

--- Server/api/index.js
+++ Server/api/index.js
@@ -5,21 +5,20 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-const db = require("./models");
+const db = require("../models");
 
 //routers
-const usersRouter = require("./routes/users");
-const petsRouter = require("./routes/pets");
-const dialogueRouter = require("./routes/dialogues");
+const usersRouter = require("../routes/users");
+const petsRouter = require("../routes/pets");
+const dialogueRouter = require("../routes/dialogues");
 
 app.use("/auth", usersRouter);
 app.use("/pets", petsRouter);
 app.use("/dialogues", dialogueRouter);
 
 db.sequelize.sync().then(() => {
-  app.listen(3001, () => {
-    console.log("Server running on port 3001");
+  const port = process.env.PORT || 3001;
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
   });
 });
-
-
