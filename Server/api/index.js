3e96e92bfe0cafdf34d670e7eab2ce7def58dd3b
tds22 <tds22@njit.edu>
2024-04-30 17:09:30 -0400

    Done

--- Server/api/index.js
+++ Server/api/index.js
@@ -5,23 +5,29 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-const db = require("../models");
+// const db = require("../models");
 
-//routers
-const usersRouter = require("../routes/users");
-const petsRouter = require("../routes/pets");
-const dialogueRouter = require("../routes/dialogues");
+// //routers
+// const usersRouter = require("../routes/users");
+// const petsRouter = require("../routes/pets");
+// const dialogueRouter = require("../routes/dialogues");
 
-app.use("/auth", usersRouter);
-app.use("/pets", petsRouter);
-app.use("/dialogues", dialogueRouter);
+// app.use("/auth", usersRouter);
+// app.use("/pets", petsRouter);
+// app.use("/dialogues", dialogueRouter);
 
-db.sequelize.sync().then(() => {
-  const port = process.env.PORT || 3001;
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
+// db.sequelize.sync().then(() => {
+//   const port = process.env.PORT || 3001;
+//   app.listen(port, () => {
+//     console.log(`Server running on port ${port}`);
+//   });
+// });
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
-
 
 module.exports = app;
